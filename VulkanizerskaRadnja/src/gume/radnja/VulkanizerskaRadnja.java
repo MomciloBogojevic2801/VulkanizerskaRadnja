6f92c98d132ed66039f44c33eb587b4eafcf3b6f
MomciloBogojevic2801 <momcilobogojevic2801@gmail.com>
2018-03-30 00:06:40 +0200

    Napisana dokumentacija za klasu VulkanizerskaRadnja

--- VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
+++ VulkanizerskaRadnja/src/gume/radnja/VulkanizerskaRadnja.java
@@ -1,12 +1,35 @@
-
 package gume.radnja;
+
+/**
+ * Klasa koja predstavlja vulkanizersku radnju.
+ * 
+ * @author Momcilo Bogojevic
+ * @version 1.0
+ *
+ */
 
 import java.util.LinkedList;
 import gume.AutoGuma;
 
 public class VulkanizerskaRadnja {
+
+	/**
+	 * Predstavlja novu listu objekata AutoGuma, pod nazivom "gume"
+	 */
 	private LinkedList<AutoGuma> gume = new LinkedList<AutoGuma>();
 
+	/**
+	 * Dodaje novu gumu u listu gume, na pocetak.
+	 * 
+	 * @param a
+	 *            predstavlja objekat klase AutoGuma
+	 * 
+	 * @throws java.lang.NullPointerException
+	 *             ako je za parametar a unet null.
+	 * 
+	 * @throws java.lang.RuntimeException
+	 *             ako je za parametar a unet objekat koji vec postoji u listi.
+	 */
 	public void dodajGumu(AutoGuma a) {
 		if (a == null)
 			throw new NullPointerException("Guma ne sme biti null");
@@ -15,6 +38,15 @@ public class VulkanizerskaRadnja {
 		gume.addFirst(a);
 	}
 
+	/**
+	 * Pronalazi sve gume iz liste gume, koje imaju istu marku i model kao uneti
+	 * parametar.
+	 * 
+	 * @param markaModel
+	 *            vrednost za atribut markaModel
+	 * @return Listu objekata tipa guma kao LinkedList
+	 * 
+	 */
 	public LinkedList<AutoGuma> pronadjiGumu(String markaModel) {
 		if (markaModel == null)
 			return null;
